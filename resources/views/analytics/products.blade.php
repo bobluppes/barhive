d80b3f134dac8522f42e97c5eb2410102e54feaa
Bob <bobluppes@live.nl>
2019-02-24 15:50:21 +0100

    Donut chart products

--- resources/views/analytics/products.blade.php
+++ resources/views/analytics/products.blade.php
@@ -19,8 +19,23 @@
             </div>
         @endforeach
     </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <i class="fa fa-bar-chart-o fa-fw"></i> Donut Chart Example
+                </div>
+                <div class="panel-body">
+                    <div id="morris-donut-chart"></div>
+                    <a href="#" class="btn btn-default btn-block">View Details</a>
+                </div>
+                <!-- /.panel-body -->
+            </div>
+        </div>
+    </div>
 @endsection
 
 @section('scripts')
-    <script src="{{ asset('js/analytics/products.blade.php') }}"></script>
+    <script src="{{ asset('js/analytics/products.js') }}"></script>
 @endsection
